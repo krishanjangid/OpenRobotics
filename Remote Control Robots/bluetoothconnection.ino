25232e99df700a5b491bce9ad6872a004b81e382
krishanjangid <krishanjangid516@gmail.com>
2022-11-22 23:51:37 +0530

    new in bluetooth

--- Remote Control Robots/bluetoothconnection.ino
+++ Remote Control Robots/bluetoothconnection.ino
@@ -1,18 +1,34 @@
-int led = 4;
 char data = 0;
-void setup() {
-  Serial.begin(9600);
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial1.begin(9600);
+    pinMode(8, OUTPUT);
 }
-
-void loop() {
-  if(Serial.available() > 0)
-  {
-    data = Serial.read();
-    if(data=='n')
-    {
-      digitalWrite(led,HIGH);
-    }
-    
-  }
-
+void loop()
+{
+   if(Serial1.available() > 0)
+   {
+      data = Serial1.read();
+      if(data == '1')
+      {
+        Serial.print(data);
+        Serial.print("=> LED ON");
+        Serial.print("\n");
+        /*Serial1.print("LED is ON");
+        Serial1.print("\n");*/        
+        digitalWrite(8, HIGH);
+      }
+         
+      else if(data == '0')
+      {
+        Serial.print(data);
+        Serial.print("=> LED OFF");
+        Serial.print("\n");
+        /*Serial1.print("LED is OFF");        
+        Serial1.print("\n");*/
+        digitalWrite(8, LOW);
+      }
+   }
 }
